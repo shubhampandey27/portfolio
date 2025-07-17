ca8eb62e9ef768af363c996d7da9573739086d51
leoraptor <sharan.kundapur@gmail.com>
2025-07-17 14:50:14 +0530

    SEO added

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.shubhfolio.com/",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+};
